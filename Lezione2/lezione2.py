c59c1fd1a27743f05997f5d152c6c90c77e04904
Nio4124 <antoniozottola41204@gmail.com>
2024-04-18 23:29:31 +0200

    cambio esercizio 3.6

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -19,7 +19,7 @@ print(f'{Albert} once said "A person who never made a mistake never tried someth
 
 #2.6 Repeat Exercise 2.5 but this time,represent the name of the vip using a variable called: famous_name
 famous_name="Kanye West"
-message="My greatest pain in life is i will never be able to see me perform"
+message="My greatest pain in life is that i will never be able to see me perform"
 print(f'{famous_name} once said "{message}"')
 
 #2.8 assign the value "python_notes.txt" to a variable called filename. then use the removesuffix() method to display the filename without th fie extension, like some browsers do.
@@ -28,5 +28,51 @@ print(filename.removesuffix('.txt'))
 
 #3.1 Store the names of a few of your friends in a list called names. print each person's name by accessing each element in the list, one at a time.
 names=["antonio", "carlo","marco"]
-for name in names:
-    print(names)
+print(names[0])
+print(names[1])
+print(names[2])
+
+#3.2 Start with the list you used in Exercise 3-1, 
+#but instead of just printing each person’s name, print a message to them. 
+#The text of each message should be the same, but each message should be personalized with the person’s name.
+print(f'how are you doing {names[0]}?')
+print(f'how are you doing {names[1]}?')
+print(f'how are you doing {names[2]}?')
+
+#3.3 Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. 
+#Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+favorite_transport=["car","motorcycle"]
+print(f'"i would like to own a honda {favorite_transport[1]}"')
+print(f'"i would be cool to own a Volkswagen {favorite_transport[0]}"')
+
+#3.4 If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner.
+#Then use your list to print a message to each person, inviting them to dinner.
+invites=["JayZ","Mike Tyson","DonaldJTrump"]
+print(f'Dear {invites[0]}, it would be a pleasure to eat dinner with us tomorrow night.')
+print(f'Dear {invites[1]}, it would be a pleasure to eat dinner with us tomorrow night.')
+print(f'Dear {invites[2]}, it would be a pleasure to eat dinner with us tomorrow night.')
+
+# 3.5 You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. 
+# You’ll have to think of someone else to invite.
+# Start with your program from Exercise 3-4. 
+# Add a print() call at the end of your program, stating the name of the guest who can’t make it.
+# Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+# Print a second set of invitation messages, one for each person who is still in your list.
+print(f'{invites[1]} cannot come to dinner.')
+invites2=["OJ Simpson","Queen Elizabeth"]
+invites.extend(invites2)
+invites.pop(1)
+print(f'Dear {invites[2]}, it would be a pleasure to eat dinner with us tomorrow night.')
+print(f'Dear {invites[3]}, it would be a pleasure to eat dinner with us tomorrow night.')
+
+#3.6 You just found a bigger dinner table, so now more space is available. 
+#Think of three more guests to invite to dinner.
+# Start with your program from Exercise 3-4 or 3-5. 
+# Add a print() call to the end of your program, informing people that you found a bigger table.
+# Use insert() to add one new guest to the beginning of your list.
+# Use insert() to add one new guest to the middle of your list.
+# Use append() to add one new guest to the end of your list.
+# Print a new set of invitation messages, one for each person in your list.
+print(f'Dear {invites[0]} {invites[1]} {invites[2]} and {invites[3]}, i have found a bigger table so i will be inviting more people to it.')
+invites.insert(0,"Micheal Jackson")
+invites.insert(2,"")
